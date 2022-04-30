9240d1204f257a76fca3f95506aad3a09f9683ae
Bohan Yang <youthdna@live.com>
2022-04-30 03:29:41 +0000

    Update modules

--- build-nginx.sh
+++ build-nginx.sh
@@ -53,22 +53,29 @@ EOF
 }
 
 NGINX_VERSION=1.21.6
+ZLIB_VERSION=1.2.12
+ZLIB_CLOUDFLARE_VERSION=959b4ea
+ZLIB_NG_VERSION=2.0.6
 PCRE_VERSION=8.45
 PCRE2_VERSION=10.39
-OPENSSL_VERSION=3.0.1
-NGX_BROTLI_VERSION=1.0.0rc
-BROTLI_VERSION=1.0.9
+OPENSSL_VERSION=3.0.2
+NGX_BROTLI_VERSION=6e975bcb015f62e1f303054897783355e2a877dc
+BROTLI_VERSION=f4153a09f87cbb9c826d8fc12c74642bb2d879ea
 NGX_GEOIP2_VERSION=3.3
 LIBMAXMINDDB_VERSION=1.6.0
+HEADERS_MORE_VERSION=0.33
+DAV_EXT_VERSION=3.0.0
 
 NGINX_URL="https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
 PCRE_URL="https://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz"
 PCRE2_URL="https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${PCRE2_VERSION}/pcre2-${PCRE2_VERSION}.tar.gz"
 OPENSSL_URL="https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz"
-NGX_BROTLI_URL="https://github.com/google/ngx_brotli/archive/refs/tags/v${NGX_BROTLI_VERSION}.tar.gz"
-BROTLI_URL="https://github.com/google/brotli/archive/refs/tags/v${BROTLI_VERSION}.tar.gz"
+NGX_BROTLI_URL="https://github.com/google/ngx_brotli/archive/${NGX_BROTLI_VERSION}.tar.gz"
+BROTLI_URL="https://github.com/google/brotli/archive/${BROTLI_VERSION}.tar.gz"
 NGX_GEOIP2_URL="https://github.com/leev/ngx_http_geoip2_module/archive/refs/tags/${NGX_GEOIP2_VERSION}.tar.gz"
 LIBMAXMINDDB_URL="https://github.com/maxmind/libmaxminddb/releases/download/${LIBMAXMINDDB_VERSION}/libmaxminddb-${LIBMAXMINDDB_VERSION}.tar.gz"
+HEADERS_MORE_URL="https://github.com/openresty/headers-more-nginx-module/archive/refs/tags/v${HEADERS_MORE_VERSION}.tar.gz"
+DAV_EXT_URL="https://github.com/arut/nginx-dav-ext-module/archive/refs/tags/v${DAV_EXT_VERSION}.tar.gz"
 
 CONFIGURE_ARGS="--prefix=/etc/nginx \
 --sbin-path=/usr/sbin/nginx \
@@ -115,14 +122,20 @@ fetch_source "nginx-${NGINX_VERSION}" "$NGINX_URL"
 with_lib pcre "pcre2-${PCRE2_VERSION}" "$PCRE2_URL"
 with_lib openssl "openssl-${OPENSSL_VERSION}" "$OPENSSL_URL"
 
-#with_zlib madler 1.2.11
-#with_zlib cloudflare 959b4ea
-with_zlib ng 2.0.6
+#with_zlib madler "$ZLIB_VERSION"
+#with_zlib cloudflare "$ZLIB_CLOUDFLARE_VERSION"
+with_zlib ng "$ZLIB_NG_VERSION"
 
 fetch_source "ngx_brotli-${NGX_BROTLI_VERSION}" "$NGX_BROTLI_URL"
 
 fetch_source "ngx_brotli-${NGX_BROTLI_VERSION}/deps/brotli" "$BROTLI_URL"
 CONFIGURE_ARGS="$CONFIGURE_ARGS --add-module=../ngx_brotli-${NGX_BROTLI_VERSION}"
+
+fetch_source "headers-more-nginx-module-${HEADERS_MORE_VERSION}" "$HEADERS_MORE_URL"
+CONFIGURE_ARGS="$CONFIGURE_ARGS --add-module=../headers-more-nginx-module-${HEADERS_MORE_VERSION}"
+
+fetch_source "nginx-dav-ext-module-${DAV_EXT_VERSION}" "$DAV_EXT_URL"
+CONFIGURE_ARGS="$CONFIGURE_ARGS --add-module=../nginx-dav-ext-module-${DAV_EXT_VERSION}"
 
 cd "nginx-${NGINX_VERSION}"
 
